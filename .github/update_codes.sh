537100ad45721db54db95380eb57008cd3d16cb7
Ace_Radom <sichenradomlyu@gmail.com>
2024-10-09 16:25:45 +0200

    add workflow

---
+++ .github/update_codes.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+cd $1
+# enter install dir
+
+if [ ! -d "openvpn-mgmt" ]; then
+    git clone --recursive https://github.com/Ace-Radom/openvpn-mgmt.git
+else
+    cd openvpn-mgmt
+    git pull
+    git submodule update --recursive
+fi
+
+logout
