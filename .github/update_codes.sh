b43500293ae12c9c66b7976200dbc38c58d7dbf3
Ace_Radom <sichenradomlyu@gmail.com>
2024-10-10 15:08:10 +0200

    write msg to log when updated

--- .github/update_codes.sh
+++ .github/update_codes.sh
@@ -5,9 +5,19 @@ cd $1
 
 if [ ! -d "openvpn-mgmt" ]; then
     git clone --recursive https://github.com/Ace-Radom/openvpn-mgmt.git
+    cd openvpn-mgmt
 else
     cd openvpn-mgmt
     git fetch --all
     git reset --hard origin/main
     git submodule update --recursive
 fi
+
+log_file="/var/openvpn-mgmt/log/openvpn-mgmt.0.log"
+if [[ -f "$log_file" ]]; then
+    main_commit=$(git log -1 --pretty=format:%h)
+    log_msg_header=$(printf "%-49s" "$(date '+%Y-%m-%d %H:%M:%S.%6N %Z' | awk '{print $1, $2, $3}') [gh_codeupdater]")
+    flock -x "$log_file" echo "$log_msgs_header openvpn-mgmt updated. [commit=$main_commit]" >> "$log_file"
+fi
+
+exit 0
